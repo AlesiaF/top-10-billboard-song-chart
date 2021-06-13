856da1628cc1266c315454fb8b6634b52593384a
AlesiaF <afiadulava@gmail.com>
2021-06-13 16:24:51 +0300

    traversing through response json

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ response = requests.request('GET', config.api_url, headers=api_data.headers, par
 data = response.json()
 
 print('Billboard Top 10 Hits - ' + config.api_date)
+
+for i in range(1, 11):
+    i = str(i)
+    print('Rank: ' + data['content'][i]['rank'])
+    print('Artist: ' + data['content'][i]['artist'])
+    print('Title: ' + data['content'][i]['title'])
+    print('Weeks On Chart: ' + data['content'][i]['weeks on chart'])
+    print('')
